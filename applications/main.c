e03f6b9bc4ead1c66f921a582235f94b1f52f855
Youstar <xxzwys123@163.com>
2026-01-09 00:19:36 +0800

    [add]:添加motor测试代码

--- applications/main.c
+++ applications/main.c
@@ -12,6 +12,7 @@
 #include "led.h"
 #include "key.h"
 #include "oled.h"
+#include "motor.h"
 
 extern volatile uint8_t key_flag;
 int main(int argc, char **argv)
@@ -19,14 +20,16 @@ int main(int argc, char **argv)
     LED_Init();
     KEY_IRQ_Init();
     OLED_Init();       // 初始化OLED显示屏
+    motor_pwm_init();
 
     /* OLED显示静态字符串 */
     OLED_ShowString(1, 1, "T:000-000-0");	//格式为：T:发送成功计次-发送失败计次-发送标志位
     OLED_ShowString(3, 1, "R:000-000-0");	//格式为：R:接收成功计次-接收失败计次-接收标志位
     while (1) {
-
-        LED_Toggle();
-        rt_thread_mdelay(1000);
+        left_motor(20);
+        right_motor(20);
+        //LED_Toggle();
+        //rt_thread_mdelay(1000);
     }
 
 }
